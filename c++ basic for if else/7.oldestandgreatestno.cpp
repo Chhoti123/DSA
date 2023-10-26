5435d39770c089151f699b7a28072aef5c0e3e3a
Shweta Chaudhary <shweta.chaudhary@kelltontech.org>
2023-10-26 22:58:03 +0530

    dsaIifelse

---
+++ c++ basic for if else/7.oldestandgreatestno.cpp
@@ -0,0 +1,17 @@
+// Take input of age of 3 people by user and determine oldest and youngest among them.
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter first people age";
+    cin>>a;
+    cout<<"Enter second people age";
+    cin>>b;
+    cout<<"Enter third people age";
+    cin>>c;
+    if(a){
+
+    }
+    return 0;
+
+}
